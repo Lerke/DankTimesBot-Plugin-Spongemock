8e9ff5ae0136b4baf7fdf6c355a5972efd81bf85
Martin Donkersloot <martin.donkersloot@outlook.com>
2018-08-26 23:18:54 +0200

    Updated bot to work with final 1.7.0 plugins architecture.

--- plugin.ts
+++ plugin.ts
@@ -1,31 +1,42 @@
+import { BotCommand } from "../../src/bot-commands/bot-command";
+import { Chat } from "../../src/chat/chat";
+import { User } from "../../src/chat/user/user";
 import { AbstractPlugin } from "../../src/plugin-host/plugin/plugin";
-import { ChatMessage } from "../../src/chat/chat-message/chat-message";
 
-export class Plugin extends AbstractPlugin
-{
-  constructor()
-  {
-    super("Spongemock", "1.0.0", {});
+export class Plugin extends AbstractPlugin {
 
-    this.registerCommand("spongemock", (params: ChatMessage) => 
-    {
-      return [ 
-        (params.reply_text.length > 0) 
-        ? this.spongmockify(params.reply_text)
-        : this.spongmockify(params.text) 
-      ];
-    });
-
+  constructor() {
+    super("Spongemock", "1.0.0");
     console.log("Loaded Spongemock!");
   }
 
-  private spongmockify(_message: string): string {
-      let spaces: number = 0;
-      const mockery = (value, idx) => ((idx+((value === ' ') ? spaces++ : spaces))%2) ? value.toLowerCase() : value.toUpperCase();
-
-      return `${_message
-      .split("")
-      .map(mockery)
-      .join("")}`;
+  /**
+   * @override
+   */
+  public getPluginSpecificCommands(): BotCommand[] {
+    const command = new BotCommand("spongemock", "spongemockifies a quoted message", this.spongmockify.bind(this));
+    return [command];
   }
-} 
+
+  private spongmockify(chat: Chat, user: User, msg: any, match: string[]): string {
+
+    let spaces: number = 0;
+    const mockery = (value, idx) => ((idx + ((value === " ")
+      ? spaces++ : spaces)) % 2) ? value.toLowerCase() : value.toUpperCase();
+    let message: string;
+
+    if (msg.reply_to_message) {
+      message = msg.reply_to_message.text;
+    } else {
+      const textSplit = (msg.text as string).split(" ");
+      message = textSplit.slice(1, textSplit.length).join(" ");
+    }
+
+    if (message) {
+      return `${message
+        .split("")
+        .map(mockery)
+        .join("")}`;
+    }
+  }
+}
