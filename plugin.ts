c138173339ae09e8ac6f0c45e47779640fd9197b
Willem <willem.sporen@scenius.nl>
2021-10-12 22:39:42 +0200

    GH-6, GH-4, GH-2

--- plugin.ts
+++ plugin.ts
@@ -2,41 +2,76 @@ import { BotCommand } from "../../src/bot-commands/bot-command";
 import { Chat } from "../../src/chat/chat";
 import { User } from "../../src/chat/user/user";
 import { AbstractPlugin } from "../../src/plugin-host/plugin/plugin";
+import TelegramBot from "node-telegram-bot-api";
 
 export class Plugin extends AbstractPlugin {
+  private specialCasingMap = {
+    '?': "¿",
+    "!": "¡",
+    ".": "·",
+    ",": "'",
+    ";": "⁏",
+    "/": "\\",
+    "\\": "/",
+  }
 
   constructor() {
-    super("Spongemock", "1.0.0");
-    console.log("Loaded Spongemock!");
+    super("Spongemock", "1.0.1");
   }
 
   /**
    * @override
    */
   public getPluginSpecificCommands(): BotCommand[] {
-    const command = new BotCommand("spongemock", "spongemockifies a quoted message", this.spongmockify.bind(this));
+    const command = new BotCommand(["spongemock", "🧽"], "spongemockifies a quoted message", this.spongmockify.bind(this));
     return [command];
   }
 
-  private spongmockify(chat: Chat, user: User, msg: any, match: string[]): string {
-
+  private spongmockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+    var initialCase = (Math.random() < 0.5) ? 0 : 1;
     let spaces: number = 0;
-    const mockery = (value, idx) => ((idx + ((value === " ")
-      ? spaces++ : spaces)) % 2) ? value.toLowerCase() : value.toUpperCase();
-    let message: string;
+    const mockery = (value: string, idx: number) => ((initialCase + idx + ((value === " ")
+        ? spaces++ : spaces)) % 2)
+        ? (this.isSpecialCharacter(value) ? this.convertSpecial(value, true) : value.toLowerCase())
+        : (this.isSpecialCharacter(value) ? this.convertSpecial(value, false) : value.toUpperCase());
 
+    let message = "";
     if (msg.reply_to_message) {
-      message = msg.reply_to_message.text;
+      message = msg.reply_to_message.text ?? "";
     } else {
       const textSplit = (msg.text as string).split(" ");
       message = textSplit.slice(1, textSplit.length).join(" ");
     }
 
-    if (message) {
-      return `${message
+    return `${(message ?? "")
         .split("")
         .map(mockery)
         .join("")}`;
+  }
+
+  private isSpecialCharacter(character: string): boolean {
+    return Object.keys(this.specialCasingMap)
+        .concat(Object.values(this.specialCasingMap))
+        .find(x => x == character) != undefined;
+  }
+
+  private convertSpecial(character: string, leftToRight: boolean): string {
+    var r = character;
+
+    if (leftToRight) {
+      // @ts-ignore
+      r = this.specialCasingMap[character] ?? r;
+    } else {
+      let values = Object.keys(this.specialCasingMap)
+          .filter(f => {
+            // @ts-ignore
+            return this.specialCasingMap[f] === character
+          });
+      if (values.length > 0) {
+        r = values[0];
+      }
     }
+
+    return r;
   }
 }
