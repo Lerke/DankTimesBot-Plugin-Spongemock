c575d873c9b3e248ba2ccd8cff670af5fd89a989
DESKTOP-2OGVFBC\maiko <maiko.kingma@outlook.com>
2022-07-16 13:30:20 +0200

    Add miako mock

--- plugin.ts
+++ plugin.ts
@@ -2,7 +2,7 @@ import { BotCommand } from "../../src/bot-commands/bot-command";
 import { Chat } from "../../src/chat/chat";
 import { User } from "../../src/chat/user/user";
 import { AbstractPlugin } from "../../src/plugin-host/plugin/plugin";
-import TelegramBot from "node-telegram-bot-api";
+import TelegramBot, { Message } from "node-telegram-bot-api";
 
 export class Plugin extends AbstractPlugin {
   private specialCasingMap = {
@@ -25,8 +25,9 @@ export class Plugin extends AbstractPlugin {
    * @override
    */
   public getPluginSpecificCommands(): BotCommand[] {
-    const command = new BotCommand(["spongemock", "🧽", "s", "spons"], "spongemockifies a quoted message", this.spongmockify.bind(this));
-    return [command];
+    const spongeMockCmd = new BotCommand(["spongemock", "🧽", "s", "spons"], "spongemockifies a quoted message", this.spongmockify.bind(this));
+    const miakoMockCmd = new BotCommand(["miakomock", "🙏", "mm"], "miakomockifies a quoted message", this.miakoMockify.bind(this));
+    return [spongeMockCmd, miakoMockCmd];
   }
 
   private spongmockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
@@ -37,13 +38,7 @@ export class Plugin extends AbstractPlugin {
         ? (this.isSpecialCharacter(value) ? this.convertSpecial(value, true) : value.toLowerCase())
         : (this.isSpecialCharacter(value) ? this.convertSpecial(value, false) : value.toUpperCase());
 
-    let message = "";
-    if (msg.reply_to_message) {
-      message = msg.reply_to_message.text ?? "";
-    } else {
-      const textSplit = (msg.text as string).split(" ");
-      message = textSplit.slice(1, textSplit.length).join(" ");
-    }
+    var message = this.getMsgToMockify(msg);
 
     return `${(message ?? "")
         .split("")
@@ -75,5 +70,63 @@ export class Plugin extends AbstractPlugin {
     }
 
     return r;
+  }
+
+  private getMsgToMockify(msg: Message) {
+    if (msg.reply_to_message) {
+      return msg.reply_to_message.text ?? "";
+    } else {
+      const textSplit = (msg.text as string).split(" ");
+      return textSplit.slice(1, textSplit.length).join(" ");
+    }
+  }
+
+  private miakoMockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+    var message = this.getMsgToMockify(msg);
+
+    if (!message)
+      return "";
+    
+    const indexesToSwap = new Array<number>();
+    let msgArr = message.split("");
+    msgArr = msgArr.map((value: string, idx: number) => {
+      let swapChance = 0.1;
+      if (value === " ") {
+        if (msgArr[idx - 3] && msgArr[idx - 3] !== " " &&
+          msgArr[idx - 2] && msgArr[idx - 2] !== " " &&
+          msgArr[idx - 1] && msgArr[idx - 1] !== " " &&
+          msgArr[idx + 1] && msgArr[idx + 1] !== " " &&
+          msgArr[idx + 2] && msgArr[idx + 2] !== " " &&
+          msgArr[idx + 3] && msgArr[idx + 3] !== " ") {
+            swapChance = 0.3;
+          }
+      }
+      if (Math.random() < (value === " " ? 0.8 : 0.1))
+        indexesToSwap.push(idx);
+
+      if (value === "r")
+        return Math.random() < 0.5 ? "t" : "r";
+      if (value === "t" || value === "d") {
+        const chance = Math.random();
+        if (chance < 0.33)
+          return "d";
+        if (chance > 0.66)
+          return "t";
+        else
+          return "dt";
+      }
+      return value;
+    });
+
+    for (const index of indexesToSwap) {
+      const charToSwap = index + Math.random() > 0.5 ? 1 : -1;
+      if (!msgArr[charToSwap])
+        continue;
+      const char = msgArr[index];
+      msgArr[index] = msgArr[charToSwap];
+      msgArr[charToSwap] = char;
+    }
+
+    return msgArr.join("");
   }
 }
