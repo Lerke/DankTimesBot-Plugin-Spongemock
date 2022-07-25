9307116d8104bd2145054450294b34336a87d53f
Rick Brants <rick.brants@gmail.com>
2022-07-25 17:56:08 +0200

    Fixed consistency and changed commands

--- plugin.ts
+++ plugin.ts
@@ -18,52 +18,33 @@ export class Plugin extends AbstractPlugin {
   }
 
   constructor() {
-    super("sPoNgEmOcK", "1.0.3");
+    super("sPoNgEmOcK", "1.1.0");
   }
 
   /**
    * @override
    */
   public getPluginSpecificCommands(): BotCommand[] {
-    const spongeMockCmd = new BotCommand(["spongemock", "🧽", "s", "spons"], "spongemockifies a quoted message", this.spongemockify.bind(this));
-    const miakoMockCmd = new BotCommand(["miakomock", "🙏", "mm"], "miakomockifies a quoted message", this.miakoMockify.bind(this));
-    const sneakymockCommand = new BotCommand(["sneakymock", "💨", "sm"], "spongemockifies a quoted message and deletes your message", this.sneakymockify.bind(this));
-    return [spongeMockCmd, miakoMockCmd, sneakymockCommand];
+    const spongeMockCmd = new BotCommand(["spongemock", "sm", "🧽", "spons"], "spongemockifies a quoted message", this.spongeMockify.bind(this));
+    const miakoMockCmd = new BotCommand(["miakomock", "mm", "🙏"], "miakomockifies a quoted message", this.miakoMockify.bind(this));
+    const sneakySpongeMockCmd = new BotCommand(["sneakyspongemock", "ssm", "💨🧽"], "spongemockifies a quoted message and deletes your message", this.sneakySpongeMockify.bind(this));
+    const sneakyMiakoMockCmd = new BotCommand(["sneakymiakomock", "smm", "💨🙏"], "miakomockifies a quoted message and deletes your message", this.sneakyMiakoMockify.bind(this));
+    return [spongeMockCmd, miakoMockCmd, sneakySpongeMockCmd, sneakyMiakoMockCmd];
   }
 
-  private spongemockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+  private spongeMockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+    let textToMock = this.getTextToMock(msg);
+    let replyToMessageId = this.getReplyToMessageId(msg);
+
     var initialCase = (Math.random() < 0.5) ? 0 : 1;
     let spaces: number = 0;
-    const mockery = (value: string, idx: number) => ((initialCase + idx + ((value === " ")
+
+    let messageToSend = textToMock.split("")
+      .map((value: string, idx: number) => ((initialCase + idx + ((value === " ")
         ? spaces++ : spaces)) % 2)
         ? (this.isSpecialCharacter(value) ? value : value.toLowerCase())
-        : (this.isSpecialCharacter(value) ? this.convertSpecial(value) : value.toUpperCase());
-
-    let message = "";
-    let replyToMessageId: number | undefined = undefined;
-
-    let msgTextSplit = (msg.text as string).split(" ");
-    if (msgTextSplit.length > 1){
-      message = msgTextSplit.slice(1, msgTextSplit.length).join(" ");
-    }
-    if (msg.reply_to_message) {
-      if (message === "") {
-        if (msg.reply_to_message.text) {
-          message = msg.reply_to_message.text;
-        }
-        else if (msg.reply_to_message.caption) {
-          message = msg.reply_to_message.caption;
-        }
-      }
-      else {
-        replyToMessageId = msg.reply_to_message.message_id;
-      }
-    } 
-
-    let messageToSend = `${(message ?? "")
-        .split("")
-        .map(mockery)
-        .join("")}`;
+        : (this.isSpecialCharacter(value) ? this.convertSpecialCharacter(value) : value.toUpperCase()))
+      .join("");
 
     if (messageToSend) {
       this.sendMessage(chat.id, messageToSend, replyToMessageId, false);
@@ -72,76 +53,23 @@ export class Plugin extends AbstractPlugin {
     return "";
   }
 
-  private sneakymockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
-    this.spongemockify(chat, user, msg, match);
-
-    this.deleteMessage(chat.id, msg.message_id);
-
-    return "";
-  }
-
-  private isSpecialCharacter(character: string): boolean {
-    return Object.keys(this.specialCasingMap)
-        .concat(Object.values(this.specialCasingMap))
-        .find(x => x == character) != undefined;
-  }
-
-  private convertSpecial(character: string): string {
-    var r = character;
-
-    let valueByKey = this.specialCasingMap[r];
-    let keyByValue = Object.keys(this.specialCasingMap).find(key => this.specialCasingMap[key] === r);
-
-    if (valueByKey != undefined) {
-      return valueByKey;
-    }
-    else if (keyByValue != undefined){
-      return keyByValue
-    }
-    
-    return r;
-  }
-
-  private getMsgToMockify(msg: Message) {
-    if (msg.reply_to_message) {
-      return msg.reply_to_message.text ?? "";
-    } else {
-      const textSplit = (msg.text as string).split(" ");
-      return textSplit.slice(1, textSplit.length).join(" ");
-    }
-  }
-
   private miakoMockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+    let textToMock = this.getTextToMock(msg);
+    let replyToMessageId = this.getReplyToMessageId(msg);
 
-    let message = "";
-    let replyToMessageId: number | undefined = undefined;
-
-    let msgTextSplit = (msg.text as string).split(" ");
-    if (msgTextSplit.length > 1){
-      message = msgTextSplit.slice(1, msgTextSplit.length).join(" ");
-    }
-    if (msg.reply_to_message) {
-      if (message === "") {
-        message = msg.reply_to_message.text ?? "";
-      }
-      else {
-        replyToMessageId = msg.reply_to_message.message_id;
-      }
-    } 
-    
     const indexesToSwap = new Array<number>();
-    let msgArr = message.split("");
-    msgArr = msgArr.map((value: string, idx: number) => {
+    let textToMockArray = textToMock.split("");
+    textToMockArray = textToMockArray.map((value: string, idx: number) => {
       let swapChance = 0.1;
       if (value === " ") {
-        if (msgArr[idx - 3] && msgArr[idx - 3] !== " " &&
-          msgArr[idx - 2] && msgArr[idx - 2] !== " " &&
-          msgArr[idx - 1] && msgArr[idx - 1] !== " " &&
-          msgArr[idx + 1] && msgArr[idx + 1] !== " " &&
-          msgArr[idx + 2] && msgArr[idx + 2] !== " " &&
-          msgArr[idx + 3] && msgArr[idx + 3] !== " ") {
-            swapChance = 0.3;
-          }
+        if (textToMockArray[idx - 3] && textToMockArray[idx - 3] !== " " &&
+          textToMockArray[idx - 2] && textToMockArray[idx - 2] !== " " &&
+          textToMockArray[idx - 1] && textToMockArray[idx - 1] !== " " &&
+          textToMockArray[idx + 1] && textToMockArray[idx + 1] !== " " &&
+          textToMockArray[idx + 2] && textToMockArray[idx + 2] !== " " &&
+          textToMockArray[idx + 3] && textToMockArray[idx + 3] !== " ") {
+          swapChance = 0.3;
+        }
       }
       if (Math.random() < (value === " " ? 0.8 : 0.1))
         indexesToSwap.push(idx);
@@ -162,18 +90,80 @@ export class Plugin extends AbstractPlugin {
 
     for (const index of indexesToSwap) {
       const charToSwap = index + Math.random() > 0.5 ? 1 : -1;
-      if (!msgArr[charToSwap])
+      if (!textToMockArray[charToSwap])
         continue;
-      const char = msgArr[index];
-      msgArr[index] = msgArr[charToSwap];
-      msgArr[charToSwap] = char;
+      const char = textToMockArray[index];
+      textToMockArray[index] = textToMockArray[charToSwap];
+      textToMockArray[charToSwap] = char;
     }
 
-    const messageToSend = msgArr.join("");
+    const messageToSend = textToMockArray.join("");
     if (messageToSend) {
       this.sendMessage(chat.id, messageToSend, replyToMessageId, false);
     }
 
     return "";
+  }
+
+  private sneakySpongeMockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+    this.spongeMockify(chat, user, msg, match);
+
+    this.deleteMessage(chat.id, msg.message_id);
+
+    return "";
+  }
+
+  private sneakyMiakoMockify(chat: Chat, user: User, msg: TelegramBot.Message, match: string): string {
+    this.miakoMockify(chat, user, msg, match);
+
+    this.deleteMessage(chat.id, msg.message_id);
+
+    return "";
+  }
+
+  private getTextToMock(msg: TelegramBot.Message): string {
+    let msgTextSplit = (msg.text as string).split(" ");
+    if (msgTextSplit.length > 1) {
+      return msgTextSplit.slice(1, msgTextSplit.length).join(" ");
+    }
+    else if (msg.reply_to_message?.text) {
+      return msg.reply_to_message.text;
+    }
+    else if (msg.reply_to_message?.caption) {
+      return msg.reply_to_message.caption;
+    }
+
+    return "";
+  }
+
+  private getReplyToMessageId(msg: TelegramBot.Message): number | undefined {
+    let msgTextSplit = (msg.text as string).split(" ");
+    if (msgTextSplit.length > 1 && msg.reply_to_message) {
+      return msg.reply_to_message.message_id;
+    }
+
+    return undefined;
+  }
+
+  private isSpecialCharacter(character: string): boolean {
+    return Object.keys(this.specialCasingMap)
+      .concat(Object.values(this.specialCasingMap))
+      .find(x => x == character) != undefined;
+  }
+
+  private convertSpecialCharacter(character: string): string {
+    var r = character;
+
+    let valueByKey = this.specialCasingMap[r];
+    let keyByValue = Object.keys(this.specialCasingMap).find(key => this.specialCasingMap[key] === r);
+
+    if (valueByKey != undefined) {
+      return valueByKey;
+    }
+    else if (keyByValue != undefined) {
+      return keyByValue
+    }
+
+    return r;
   }
 }
